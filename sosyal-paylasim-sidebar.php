a5f0f65c9587de374e5df2d60b431a2babe5a833
Akın Ayturan <akinayturan@gmail.com>
2015-02-12 23:35:37 +0200

    bug fixes

--- sosyal-paylasim-sidebar.php
+++ sosyal-paylasim-sidebar.php
@@ -1,10 +1,10 @@
 <div class="sosyal">
-    <a rel="external publisher" class="gplus" href="https://plus.google.com/102424977791408310221" title="G-plus-EP"></a>
-    <a rel="external" class="facebook" href="//www.facebook.com/epfarki" title="Facebook-EP"></a>
-    <a rel="external" class="twitter" href="//twitter.com/epfarki" title="Twitter-EP"></a>
-    <a rel="external" class="pinterest" href="//pinterest.com/epfarki/epfarki-com/" title="Pinterest-EP"></a>
-    <a rel="external" class="linkedin" href="//linkedin.com/in/bizimakin/" title="Linkedin-EP"></a>
-    <a rel="external" class="youstube" href="//www.youtube.com/user/epfarki" title="Youtube-EP"></a>
-    <a rel="external" class="rss" href="//feeds.feedburner.com/datarar" title="Feedburner-EP"></a>
-    <a rel="external" class="rss-mail" href="//feedburner.google.com/fb/a/mailverify?uri=epfarki" title="Mail-EP"></a>
+    <a rel="external publisher" class="gplus" href="https://plus.google.com/103708753654364217686" title="G-plus-DR"></a>
+    <a rel="external" class="facebook" href="//www.facebook.com/datarar" title="Facebook-DR"></a>
+    <a rel="external" class="twitter" href="//twitter.com/datarar_com" title="Twitter-DR"></a>
+    <a rel="external" class="pinterest" href="//www.pinterest.com/datarar/datararcom/" title="Pinterest-DR"></a>
+    <a rel="external" class="linkedin" href="//www.linkedin.com/company/datarar" title="Linkedin-DR"></a>
+    <a rel="external" class="youstube" href="//www.youtube.com/channel/UCpGJ2flXKGcawIfZ1UDiR9Q" title="Youtube-DR"></a>
+    <a rel="external" class="rss" href="//feeds.feedburner.com/datarar" title="Feedburner-DR"></a>
+    <a rel="external" class="rss-mail" href="//feedburner.google.com/fb/a/mailverify?uri=datarar" title="Mail-DR"></a>
 </div>
