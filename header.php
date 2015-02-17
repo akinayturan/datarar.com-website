3ec2b874673d9c965ff961acb985d7f1124ffe8c
Akın Ayturan <akinayturan@gmail.com>
2015-02-17 23:25:35 +0200

    minor improvement

--- header.php
+++ header.php
@@ -30,7 +30,18 @@
                     <h1><a title="EP" href="//datarar.com/"> <span>Data</span>Rar</a></h1>
                 </div>
             </div>
-            <?php include (TEMPLATEPATH . "/menu-ust.php"); ?>
+            <div id="ustkategori" class="menunav">
+                <ul>
+                    <li><a href="//datarar.com/blog/" title="Blog">Blog</a></li>
+                    <li><a href="//datarar.com/hizmetler/" title="Hizmetler">Hizmetler</a></li>
+                    <li><a href="//datarar.com/inceleme/" title="İnceleme">İnceleme</a></li>
+                    <li><a href="//datarar.com/hakkimizda/" title="Hakkımızda">Hakkımızda</a></li>
+                    <li><a href="//datarar.com/ziyaretci-defteri/" title="Ziyaretçi Defteri">Ziyaretçi Defteri</a></li>
+                    <li><a href="//datarar.com/reklam-ver/" title="Reklam">Reklam</a></li>
+                    <li><a href="//datarar.com/iletisim/" title="İletişim">İletişim</a></li>
+                    <li id="ustmenuson"><a rel="appendix" href="//datarar.com/?rastgele" title="Rastgele Yazı">Rastgele</a></li>
+                </ul>
+            </div>
             <?php include (TEMPLATEPATH . "/searchform.php"); ?>
         </div>
     </div>
