309c1ef06300869001385a12bfc734f0a7ba04f6
Akın Ayturan <akinayturan@gmail.com>
2015-02-26 16:21:50 +0200

    minor improvement

--- hizmet/tema/menu3.php
+++ hizmet/tema/menu3.php
@@ -4,12 +4,12 @@
 </div>
 <div id="sidebar">
 
+    <? require('../../wp-content/themes/datarar/sosyal-paylasim-sidebar.php'); ?>
+
     <div id="anakategorimenu">
         <ul><li id="sozler"><a><i>Albert Einstein</i>: <cite>Mantık sizi A'dan B'ye götürür... Hayalgücü ise her yere...</cite></a></li></ul>
     </div>
 
-    <? require('../../wp-content/themes/datarar/sosyal-paylasim-sidebar.php'); ?>
-    <div class="temiz"></div>
     <div class="yans yans2">
         <h5><span>Son Yazılar</span></h5>
         <ul>
@@ -18,39 +18,47 @@
     </div>
 
 </div>
+
+
+</div></div></div>
 <div id="footer-outer" class="temiz">
-    <div class="footerout" class="temiz">
+    <div class="footerout">
 
         <div id="footer-bottom">
             <div class="bottom-left"><p>
-                    <a href="//datarar.com/hizmet/" rel="appendix" title="Hizmet">Hizmet</a> |
+                    <a href="//datarar.com/hizmetler/" rel="appendix" title="Hizmet">Hizmet</a> |
                     <a href="//feeds.feedburner.com/datarar" rel="external" title="Besleme">Feed</a> |
                     <a href="//www.alexa.com/siteinfo/datarar.com" rel="external" title="Alexa">Alexa</a> |
-                    <a href="//validator.w3.org/check?uri=http%3a%2f%2fdatarar.com%2f" rel="external" title="W3-html">W3-html</a> |
-                    <a href="//jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Fdatarar.com%2F" rel="external" title="W3-css">W3-css</a> |
-                    <a href="//datarar.com/sitemap-video.xml" rel="external" title="Video-Site Haritası">V-Site Haritası</a> |
+                    <a href="//datarar.com/sitemap-video.xml" rel="external" title="Video-Site Haritası">Video Haritası</a> |
                     <a href="//datarar.com/site-haritasi/" title="Site Haritası">Site Haritası</a>
                 </p></div>
 
             <div class="bottom-right"><p>
-                    <a href="//datarar.com/" title="datarar.com">Datarar</a> <a href="//datarar.com/git/?url=//creativecommons.org/licenses/by-nc-nd/3.0/deed.tr" title="creativecommons" rel="external">©2013</a> | Tasarım: <a href="//datarar.com/iletisim/" title="İletişim">Bizimakin</a> | Özel İletişim: iletisim [at] datarar.com
+                    Datarar <a href="//creativecommons.org/licenses/by-nc-nd/4.0/deed.tr" title="creativecommons" rel="external">©2015</a> | Tasarım: <a href="//datarar.com/iletisim/" title="İletişim">Bizimakin</a> | Özel İletişim: iletisim [at] datarar.com
                 </p></div>
         </div>
 
 
-        <div id="sosyal10"><a class="yukari updatable" href="#"></a></div>
-        <a class="gulucuk"></a>
-    </div></div></div></div></div></div>
+        <div style="margin-left: 25px;float: left; width: 250px !important;height: 50px !important;">
+            <div style="display: block;">
+                <a href="http://bumerang.hurriyet.com.tr" class="BoomadsButtonLink154" target="_blank"><img src="http://widget.boomads.com/images/bumerangWidget/bumerang-25050-white.gif" alt="Bumerang - Yazarkafe"/></a>
+            </div>
+        </div>
+        <div id="sosyal10"><a href="#" class="updatable"><i class="fa fa-caret-up  fa-2x"></i></a></div>
+    </div></div>
+</div>
 
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <script src="//www.google.com.tr/coop/cse/brand?form=cse-search-box&amp;lang=tr"></script>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
 <script type="text/javascript" language="javascript" src="http://datarar.com/dr/tema/js/parser.js"></script>
 <script type="text/javascript">
     $(function(){
         parseRSS('http://datarar.com/feed/', '#datarar')
     });
 </script>
+
 <script src="//datarar.com/dr/tema/js/sozler.js"></script>
 <script type="text/javascript">
     document.getElementById('preloader').className += 'loading';
